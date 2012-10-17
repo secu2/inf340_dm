344fbde8fc52f78a90b300167b336e54c023f474
Clara <p0up0umpiid0u@gmail.com>
2012-10-17 08:57:24 +0200

    Supprime

--- inf340_dm/index.php
+++ inf340_dm/index.php
@@ -8,7 +8,6 @@ define('DEBUGGING', false);
  * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
  *
- *
  * You can load different configurations depending on your
  * current environment. Setting the environment also influences
  * things like logging and error reporting.
