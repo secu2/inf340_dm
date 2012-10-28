0a57c1b3610e3b5eba61019d03d17963fb161001
Marie <Marie@Marie-PC>
2012-10-28 19:11:29 +0100

    Modifs galerie et création de la view pour les stations

---
+++ inf340_dm/application/views/frontoffice/station_info_view.php
@@ -0,0 +1,27 @@
+<!-- Affiche les infos d'une station -->
+<h2>Nom de la station</h2>
+<table>
+	<tr>
+		<th>D&eacute;partement</th>
+		<td><?php ?></td>
+	</tr>
+	<tr>
+		<th>Photo</th>
+		<td><?php ?></td>
+	</tr>
+</table>
+<h2>Commentaires</h2>
+<table>
+	<?php ?>
+	<tr>
+		<!-- Pseudo de la personne qui a posté un commentaire -->
+		<td><?php ?></td>
+		<!-- Le commentaire -->
+		<td><?php ?></td>
+		<!-- Si l'utilisateur est un admin ou un modo, on ajoute un bouton pour supprimer le commentaire -->
+		<?php ?>
+		<td><?php ?></td>
+		<?php ?>
+	</tr>
+	<?php ?>
+</table>
--- inf340_dm/application/views/frontoffice/stations_view.php
+++ inf340_dm/application/views/frontoffice/stations_view.php
@@ -1,1 +1,19 @@
+<!-- Affiche la liste complète des galeries -->
 <h1>Les stations</h1>
+<!-- Cliquer sur le nom ou la photo d'une station envoie vers sa fiche -->
+<?php ?>
+<table>
+	<tr>
+		<!-- Nom de la station -->
+		<th><?php ?></th>
+	</tr>
+	<tr>
+		<!-- Photo de la station -->
+		<td><?php ?></td>
+	</tr>
+	<tr>
+		<!-- Si l'utilisateur est un admin, on ajoute un bouton pour supprimer la station -->
+		<td><?php ?></td>
+	</tr>
+</table>
+<?php ?>
