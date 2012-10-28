fb59b78d6a3f36fd4ad02dee55cecb983a9aff75
Marie <Marie@Marie-PC>
2012-10-28 19:35:48 +0100

    Création pages "votre compte" et gestion utilisateur

---
+++ inf340_dm/application/views/backoffice/compte_user_view.php
@@ -0,0 +1,16 @@
+<h2>Votre compte</h2>
+<!-- L'utilisateur peut modifier son login et son mot de passe -->
+<table>
+	<tr>
+		<th>Login</th>
+		<td><?php ?></td>
+	</tr>
+	<tr>
+		<th>Mot de passe</th>
+		<td><?php ?></td>
+	</tr>
+	<tr>
+		<th>Nombre de commentaires</th>
+		<td><?php ?></td>
+	</tr>
+</table>
---
+++ inf340_dm/application/views/backoffice/gestion_user_view.php
@@ -0,0 +1,16 @@
+<!-- Si l'utilisateur est un admin, la liste des utilisateurs sera affichée sur son profil -->
+<!-- Il pourra ainsi supprimer des utilisateurs, les nommer modérateurs, ou les rétrograder -->
+<h2>Liste des utilisateurs</h2>
+<table>
+	<?php ?>
+	<tr>
+		<!-- Login -->
+		<td><?php ?></td>
+		<!-- Si c'est un utlisateur normal, on ajoute un bouton "nommer modérateur" -->
+		<!-- Si c'est un modérateur, on ajoute un bouton "rétrograder" -->
+		<td><?php ?></td>
+		<!-- Bouton "supprimer utilisateur" -->
+		<td><?php ?></td>
+	</tr>
+	<?php ?>
+</table>
