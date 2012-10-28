06bfa3732d7590f7c42087dc3be83a6add8eade9
Clara <p0up0umpiid0u@gmail.com>
2012-10-28 16:52:37 +0100

    Test CSS

--- inf340_dm/application/views/templates/header.php
+++ inf340_dm/application/views/templates/header.php
@@ -5,7 +5,7 @@
     <head>    
     <title> Dahu </title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   
+	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 	</head>
 	<body>
 		<p>
@@ -30,6 +30,8 @@
 				<th>News</th>
 			</tr>
 			<tr>
-				<td>01/01/01 - Ouverture du site</td>
+				<td> 01/01/01 - Ouverture du site</td>
 			</tr>
-		</table><hr>
+		</table>
+	</body>
+</html>
