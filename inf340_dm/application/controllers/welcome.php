11be5e4e27157c2de010caec9b434cae5e73a610
Marie <Marie@Marie-PC>
2012-11-06 20:37:52 +0100

    Mise en place du lien vers la page Votre Compte

--- inf340_dm/application/controllers/welcome.php
+++ inf340_dm/application/controllers/welcome.php
@@ -57,6 +57,18 @@ class Welcome extends CI_Controller {
 		$this->load->view('frontoffice/stations_view');
 		$this->load->view('templates/footer');
 	}
+	
+	public function compte()
+	{
+		$this->load->helper('url');
+		//Si l'utilisateur n'est pas connecté, on le renvoit sur la page de connexion avec un message d'erreur
+		$this->load->view('backoffice/templates/header_connexion');
+		$this->load->view('backoffice/login_needed_view');
+		$this->load->view('modules/login');
+		//Sinon on l'envoit sur la page de son compte
+		
+		$this->load->view('templates/footer');
+	}
 }
 
 /* End of file welcome.php */
--- inf340_dm/application/views/templates/header.php
+++ inf340_dm/application/views/templates/header.php
@@ -32,7 +32,7 @@
 		<ul id="menu">
 			<li><a href=<?php echo site_url();?>>Accueil</a></li>
 			<li><a href=<?php echo site_url('welcome/stations/');?>>Les stations</a></li>
-			<li><a href="">Votre compte</a></li>
+			<li><a href=<?php echo site_url('welcome/compte/');?>>Votre compte</a></li>
 		</ul>
 		
 	</body>
