6a0297b779790a381bc1b64779ff5be56c0b7889
Marie <Marie@Marie-PC>
2012-11-09 10:04:07 +0100

    Modifs

--- inf340_dm/application/views/index.php
+++ inf340_dm/application/views/index.php
@@ -1,2 +1,2 @@
-<h1>Bienvenu sur le site du Dahu !</h1>
+<h1>Bienvenue sur le site du Dahu !</h1>
 <p>Inscrivez-vous et retrouvez la liste des stations de ski de votre r&eacute;gion, comment&eacute;es par nos membres.</p><hr>
