cd53ad6beb71fa39cd14f61316cd48095b76883d
Clément Brocard <clementbrocard@192.168.2.1>
2012-11-09 01:11:38 +0100

    Modif de la colonne texte en data

--- inf340_dm/application/models/repositories/CommentaireRepository.php
+++ inf340_dm/application/models/repositories/CommentaireRepository.php
@@ -2,14 +2,14 @@
 namespace models\repositories;
 use Doctrine\Common\Collections\ArrayCollection;
 /**
- * Le dépôt utilisateur
+ * Le dï¿½pï¿½t utilisateur
  * @author jub
  *
  */
 class CommentaireRepository extends \Doctrine\ORM\EntityRepository {
 
-	public function create($id,$nom, $texte){
-		$commentaire = new \models\Commentaire($id,$nom, $texte);
+	public function create($id,$nom, $data){
+		$commentaire = new \models\Commentaire($id,$nom,$data);
 		$this->getEntityManager()->persist($commentaire);
 		$this->getEntityManager()->flush();
 		return $commentaire;
@@ -18,20 +18,20 @@ class CommentaireRepository extends \Doctrine\ORM\EntityRepository {
 	public function getUtilisateurByStation($nom){
 		
 		$res = new ArrayCollection();
-		foreach ($this->findByNom($nom) as $texte)
-			$res->add($texte->getId());
+		foreach ($this->findByNom($nom) as $data)
+			$res->add($data->getId());
 		return $res;
 	}
 	
 	public function getStationByUtilisateur($id){
 	
 		$res = new ArrayCollection();
-		foreach ($this->findById($id) as $texte)
-			$res->add($texte->getNom());
+		foreach ($this->findById($id) as $data)
+			$res->add($data->getNom());
 		return $res;
 	}
 	
-	public function getTexteByUtilisateurStation($id,$nom){
-		return $this->findOneBy(array('id'=>$id,'nom'=>$nom))->getTexte();
+	public function getDataByUtilisateurStation($id,$nom){
+		return $this->findOneBy(array('id'=>$id,'nom'=>$nom))->getData();
 	}
 }
