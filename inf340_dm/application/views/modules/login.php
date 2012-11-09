80d9500d0a5007d483ec6f24744063c75cf63939
Clément Brocard <clementbrocard@192.168.2.1>
2012-11-09 22:45:53 +0100

    Petites modifs

--- inf340_dm/application/views/modules/login.php
+++ inf340_dm/application/views/modules/login.php
@@ -1,6 +1,6 @@
 <?php 
 	$this->load->helper('form');
-	echo form_open('/login/verify');
+	echo form_open('/user/verify');
 	echo form_input('username', 'Utilisateur');
 	echo form_password('password', 'Mot de Passe');
 	echo form_submit('submit','Connexion');
