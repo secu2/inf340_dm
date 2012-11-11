7d0bd58d09c8ef44bcc7bdd143cafd96ac162e97
Marie <Marie@Marie-PC>
2012-11-11 20:39:37 +0100

    Correction du texte d'accueil

--- inf340_dm/application/views/index.php
+++ inf340_dm/application/views/index.php
@@ -1,16 +1,15 @@
 <h1>Bienvenue sur le site du Dahu !</h1>
-<p> 
+<p>
 	<h2> Qu'est ce que le site du dahu?</h2>
-	Le site du dahu est un site de partage d'impression. Ici vous renseigner afin que votre prochaine sortie soit r&eacuteussie, 
-	ceci grace &agrave tous nos utilisateurs, et aussi gr&acircce &agrave vous qui donnent leur avis sur les diff&eacutetes stations
+	Le site du dahu est un site de partage d'impressions. Ici vous pourrez vous renseigner afin que votre prochaine sortie soit r&eacute;ussie, 
+	ceci gr&acirc;ce &agrave; tous nos utilisateurs, et aussi gr&acirc;ce &agrave; vous qui donnent leur avis sur les diff&eacute;rentes stations
 	et postent des images de celle-ci.
 	<br />
-	Tout cela vous permettant donc, d'avoir une sortie de ski r&eacuteussie la prochaine fois.
+	Tout cela vous permettant donc, d'avoir une sortie de ski r&eacute;ussie la prochaine fois.
 	
 	
-	<h2> Comment acc&eacuteder &agrave toutes ces informations?</h2>
+	<h2> Comment acc&eacute;der &agrave; toutes ces informations?</h2>
 	Inscrivez-vous et retrouvez la liste des stations de ski de votre r&eacute;gion, comment&eacute;es 
-	spar nos membres.
-	
+	par nos membres.
 </p>
 <hr>
