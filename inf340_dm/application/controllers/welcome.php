e9afaa57308f9414d6c0e1abfedd039eab03f25c
Clément Brocard <clement.secu2@free.fr>
2012-11-14 00:58:44 +0100

    J'arrive pas a faire l'upload des images trop fatigué surement... De même pour la vérif du formulaire d'jout de station ça s'annonce plus difficile que prevu. Need help! (Je prend mon ordi demain)

--- inf340_dm/application/controllers/welcome.php
+++ inf340_dm/application/controllers/welcome.php
@@ -121,7 +121,9 @@ class Welcome extends CI_Controller {
 	{
 		$this->form_validation->set_rules('nom_station', '[Nom de la station]', 'required|callback_nom_station_check');
 		$this->form_validation->set_rules('description_station', '[Description]', 'required');
-		$this->form_validation->set_rules('departement_station', '[D�partement]', 'required');
+		$this->form_validation->set_rules('userfile', '[Photo]', 'required');
+		$this->form_validation->set_rules('description_photo', '[Description Photo]', 'required');
+		$this->form_validation->set_rules('departement_station', '[D�partement]', 'required');
 	
 		if ($this->form_validation->run() == FALSE){
 			redirect(welcome/stations);
@@ -138,9 +140,62 @@ class Welcome extends CI_Controller {
 			
 			$repository = $em->getRepository('models\Station');
 			
-			$repository->create($data['nom_station'] , $data['description_station'], $data['departement_station']);
+			
+			// UPLOAD Start
+			$img_rep = $em->getRepository('models\Image');
+			$gallery_path = realpath(APPPATH . '..'.DIRECTORY_SEPARATOR.'ressources'.DIRECTORY_SEPARATOR.'images').DIRECTORY_SEPARATOR;
+			$url = temp;
+			$config['upload_path'] = '$gallery_path'; // Chemin du dossier de stockage
+			$config['allowed_types'] = 'gif|jpg|png|jpeg'; // Types d'éxtensions acceptées
+			$config['max_size']	= '10000'; // Taille maximale acceptée
+			$config['max_width']  = '1024'; // Largeur maximale
+			$config['max_height']  = '768'; // Hauteur maximale
+			$config['file_name']= $url; // Nom du fichier
+			$config['overwrite']=true; // Autorise l'écrasement
+			
+			$this->load->library('upload', $config); //Chargement librairie upload
+			
+			$upload_status = $this->upload->do_upload(); // Statut de l'upload
+			if($upload_status){ //Si l'upload marche avec ces paramètres
+				$image_data = $this->upload->data(); // On récupère l'upload
+				// Miniature Start
+				$config = array(
+						'source_image' => $image_data['full_path'], //get original image
+						'new_image' => $gallery_path, //save as new image //need to create thumbs first
+						'create_thumb' => true, //Mode thumbnail (?)
+						'maintain_ratio' => true, //mainitien le ratio
+						'width' => 75, //la largueur de la miniature sera de 250px
+						'height' => 50 //la hauteur de la miniature sera de 100px
+				);
+				$this->load->library('image_lib', $config); //Chargement lib images
+				$resize_status = $this->image_lib->resize();
+				if ($resize_status){
+					$description_image = $this->input->post('description_image');
+					$image = $img_rep->create($description_image, $url);
+					$newUrl = $image->getURL();
+					try {
+						rename($gallery_path.$url.$image_data['file_ext'],$gallery_path.$newUrl.$image_data['file_ext']);
+						rename($miniature_path.$url.$image_data['file_ext'], $miniature_path.$newUrl.'min'.$image_data['file_ext']);
+
+						$repository->create($data['nom_station'] , $data['description_station'], $data['departement_station']);
+					}
+					catch (Exception $e){
+						//en cas d'erreur on supprime l'image de la base de donnee.
+						$repository->delete($url);
+							
+					}
+					//redirection vers la page d'accueil du backoffice
+					redirect('welcome/stations');
+				}
+				// Miniature Stop
+			}
+			
+			
+			//UPLOAD Stop
+			
 			$this->load->view('notifications/add_station_ok_view');
 			$this->load->view('templates/footer');
+			
 		}
 	}
 	
@@ -153,7 +208,7 @@ class Welcome extends CI_Controller {
 			return true;
 		}
 		else{
-			$this->form_validation->set_message('nom_station_check', 'La station existe d�j�');
+			$this->form_validation->set_message('nom_station_check', 'La station existe d�j�');
 			return false;
 		}
 	}
--- inf340_dm/application/views/modules/add_station_view.php
+++ inf340_dm/application/views/modules/add_station_view.php
@@ -1,5 +1,5 @@
 <!-- Vue accessible seulement par l'admin -->
-<!-- Il peut, à partir de cette vue, ajouter une station -->
+<!-- Il peut, ï¿½ partir de cette vue, ajouter une station -->
 <h2>Ajouter une station</h2>
 <?php
 	$this->load->helper('form');
@@ -8,8 +8,8 @@
 	echo form_open('/welcome/add_station'); ?>
 <form>
 	Nom de la station : <input type="text" name="nom_station" value="<?php echo set_value('nom'); ?>" size="100" /> <br />
-	Photo : <br />
+	Photo : <input type="file" name="userfile" size="20" value="<?php echo set_value('userfile'); ?>" /> Description photo:<input type="text" name="description_image" value="<?php echo set_value('description_image'); ?>" size="10" /><br />
 	Description : <input type="text" name="description_station" value="<?php echo set_value('description'); ?>" size="100" /> <br />
-	Département : <input type="text" name="departement_station" value="<?php echo set_value('departement'); ?>" size="3"/> <br />
+	Dï¿½partement : <input type="text" name="departement_station" value="<?php echo set_value('departement'); ?>" size="3"/> <br />
 	<input type="submit" value="Submit" />
 </form>
